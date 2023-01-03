eaf29968d1365619212731c36df4b9413e7b4bca
willfcork <willfcork@gmail.com>
2023-01-03 15:58:04 -0800

    Overhaul. Added premiere_stages.py to automate all steps.

--- get_transcript.py
+++ get_transcript.py
@@ -1,143 +1,9 @@
 #!/usr/bin/env python3
 import argparse
-import json
-import logging
 import os
-import re
 import time
-from collections import Counter
-
-import xlsxwriter
 
 import assemblyai
-import utils
-
-
-def write_transcript_to_excel(transcript_json, excel_file_path):
-    """
-    Writes a transcript to an Excel file
-    """
-    workbook = xlsxwriter.Workbook(excel_file_path)
-    worksheet_words = workbook.add_worksheet("words")
-
-    # Write words
-    worksheet_words.write(0, 0, "start")
-    worksheet_words.write(0, 1, "end")
-    worksheet_words.write(0, 2, "confidence")
-    worksheet_words.write(0, 3, "speaker")
-    worksheet_words.write(0, 4, "text")
-    row = 1
-    for word in transcript_json["words"]:
-        worksheet_words.write(row, 0, utils.transcript_time_to_timecode(word["start"]))
-        worksheet_words.write(row, 1, utils.transcript_time_to_timecode(word["end"]))
-        worksheet_words.write(row, 2, word["confidence"])
-        worksheet_words.write(row, 3, word["speaker"])
-        worksheet_words.write(row, 4, word["text"])
-        row += 1
-
-    if transcript_json.get("paragraphs"):
-        worksheet_paragraphs = workbook.add_worksheet("paragraphs")
-        worksheet_paragraphs.write(0, 0, "start")
-        worksheet_paragraphs.write(0, 1, "end")
-        worksheet_paragraphs.write(0, 2, "text")
-        row = 1
-        for paragraph in transcript_json["paragraphs"]:
-            worksheet_paragraphs.write(row, 0, utils.transcript_time_to_timecode(paragraph["start"]))
-            worksheet_paragraphs.write(row, 1, utils.transcript_time_to_timecode(paragraph["end"]))
-            worksheet_paragraphs.write(row, 2, paragraph["text"])
-            row += 1
-    else:
-        logging.warning("no paragraphs were detected")
-
-    # Write highlights
-    if transcript_json["auto_highlights"]:
-        if transcript_json["auto_highlights_result"]["status"] == "success":
-            worksheet_highlights = workbook.add_worksheet("highlights")
-            worksheet_highlights.write(0, 0, "start")
-            worksheet_highlights.write(0, 1, "text")
-            worksheet_highlights.write(0, 2, "count")
-            worksheet_highlights.write(0, 3, "rank")
-            row = 1
-            for highlight in transcript_json["auto_highlights_result"]["results"]:
-                timestamps = ','.join([utils.transcript_time_to_timecode(inst["start"]) for inst in highlight["timestamps"]])
-                worksheet_highlights.write(row, 0, timestamps)
-                worksheet_highlights.write(row, 1, highlight["text"])
-                worksheet_highlights.write(row, 2, highlight["count"])
-                worksheet_highlights.write(row, 3, highlight["rank"])
-                row += 1
-        else:
-            logging.warning("auto_highlights was not successful")
-    else:
-        logging.warning("auto_highlights were not configured")
-
-    # Write chapters
-    if transcript_json["auto_chapters"]:
-        worksheet_chapters = workbook.add_worksheet("chapters")
-        worksheet_chapters.write(0, 0, "start")
-        worksheet_chapters.write(0, 1, "end")
-        worksheet_chapters.write(0, 2, "summary")
-        worksheet_chapters.write(0, 3, "gist")
-        worksheet_chapters.write(0, 4, "headline")
-        row = 1
-        for chapter in transcript_json["chapters"]:
-            worksheet_chapters.write(row, 0, utils.transcript_time_to_timecode(chapter["start"]))
-            worksheet_chapters.write(row, 1, utils.transcript_time_to_timecode(chapter["end"]))
-            worksheet_chapters.write(row, 2, chapter["summary"])
-            worksheet_chapters.write(row, 3, chapter["gist"])
-            worksheet_chapters.write(row, 4, chapter["headline"])
-            row += 1
-    else:
-        logging.warning("chapters were not detected")
-
-    # Write entities
-    if transcript_json["entity_detection"]:
-        worksheet_entities = workbook.add_worksheet("entities")
-        worksheet_entities.write(0, 0, "start")
-        worksheet_entities.write(0, 1, "end")
-        worksheet_entities.write(0, 2, "text")
-        worksheet_entities.write(0, 3, "entity_type")
-        row = 1
-        for entity in transcript_json["entities"]:
-            worksheet_entities.write(row, 0, utils.transcript_time_to_timecode(entity["start"]))
-            worksheet_entities.write(row, 1, utils.transcript_time_to_timecode(entity["end"]))
-            worksheet_entities.write(row, 2, entity["text"])
-            worksheet_entities.write(row, 3, entity["entity_type"])
-            row += 1
-    else:
-        logging.warning("no entities were detected")
-
-    # Write IAB categories
-    if transcript_json["iab_categories"]:
-        if transcript_json["iab_categories_result"]["status"] == "success":
-            worksheet_iab_categories = workbook.add_worksheet("iab_categories")
-            worksheet_iab_categories.write(0, 0, "labels")
-            worksheet_iab_categories.write(0, 1, "count")
-            row = 1
-            label_list = []
-            for iab_category in transcript_json["iab_categories_result"]["results"]:
-                for iab_label in iab_category["labels"]:
-                    for category in str(iab_label["label"]).split(">"):
-                        label_list.append(category)
-
-            counts = Counter()
-            counts.update(label_list)
-
-            for label, count in counts.items():
-                worksheet_iab_categories.write(row, 0, label)
-                worksheet_iab_categories.write(row, 1, count)
-                row += 1
-        else:
-            logging.warning("iab_categories did not succeed")
-    else:
-        logging.warning("iab_categories were not configured")
-
-    workbook.close()
-
-
-def write_transcript_to_json(transcript_json, json_file_path):
-    with open(json_file_path, "w") as f:
-        json_object = json.dumps(transcript_json, indent=4)
-        f.write(json_object)
 
 
 def parse_args():
@@ -203,10 +69,10 @@ if __name__ == '__main__':
     transcript_json["paragraphs"] = assemblyai.get_paragraphs(transcript_id)
 
     print(f"Writing {title}.xlsx")
-    write_transcript_to_excel(transcript_json, f"{output_dir}/{title}.xlsx")
+    assemblyai.write_transcript_to_excel(transcript_json, f"{output_dir}/{title}.xlsx")
 
     print(f"Writing {title}.json")
-    write_transcript_to_json(transcript_json, f"{output_dir}/{title}.json")
+    assemblyai.write_transcript_to_json(transcript_json, f"{output_dir}/{title}.json")
 
     print(f"Collecting {title}.srt")
     srtData = assemblyai.get_srt(transcript_id)
---
+++ premiere_stages.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+import tempfile
+
+import pandas as pd
+
+import assemblyai
+import utils
+from set_project_markers import clear_markers, insert_chapters
+
+
+VALID_STEPS = [1, 2, 3]
+
+TRANSCRIPT_CONFIG = {
+        "custom_spelling": [
+            {"from": ["Christina"], "to": "Krystyna"},
+            {"from": ["Krin", "Corrinne", "krin", "crin", "corinne, Karen"], "to": "Corinne"},
+            {"from": ["Antislock"], "to": "Anti-Slut"},
+            {"from": ["anti fletching"], "to": "Anti-Slut-Shaming"},
+            {"from": ["sorry about last night's show@gmail.com"], "to": "sorryaboutlastnightshow@gmail.com"}
+            ],
+        "word_boost": ["anti-slut", "anti-slut-shaming", "Guys We Fucked", "Corinne", "Krystyna", "sorryaboutlastnightshow@gmail.com"],
+        "auto_highlights": True,
+        "auto_chapters": True,
+        "entity_detection": True,
+        "iab_categories": True,
+        "speaker_labels": True
+    }
+
+
+def main(argv):
+    args = parse_args(argv)
+    steps = args.steps
+    steps.sort()
+    id_override = args.id
+    xlsx_override = args.xlsx
+
+    # Validate steps
+    for step in steps:
+        if step not in VALID_STEPS:
+            print(f"step {step} is not a valid step")
+            return -1
+
+    pymiere_proj, all_markers = utils.setup_pymiere()
+    temp_audio = None
+    xlsx_file = xlsx_override
+    transcript_id = id_override
+
+    for step in steps:
+        if step == 1:
+            print("== Extracting audio from project")
+            temp_audio, extract_result = utils.extract_project_audio(pymiere_proj)
+            print(f"  -- {extract_result}")
+            print(f"  -- Audio: {temp_audio}")
+            print("== DONE")
+        elif step == 2:
+            print("== Getting transcript")
+            if temp_audio is None and id_override is None:
+                print(f"temp_audio is missing. Please include step 1")
+                return -1
+            if transcript_id:
+                print(f"  -- Using ID override: {transcript_id}")
+            else:
+                print("  -- Uploading file")
+                upload_response = assemblyai.upload_file(temp_audio)
+                audio_url = upload_response["upload_url"]
+                transcript_response = assemblyai.get_transcript(audio_url, TRANSCRIPT_CONFIG)
+                transcript_id = transcript_response["id"]
+                print(f"  -- Transcript ID: {transcript_id}")
+            print("  -- Waiting for data")
+            transcript_json = assemblyai.poll_for_transcript(transcript_id, log=False)
+            print("  -- Data ready")
+            print("  -- Saving transcript.xlsx")
+            xlsx_file = os.path.join(tempfile.mkdtemp(), "transcript.xlsx")
+            assemblyai.write_transcript_to_excel(transcript_json, xlsx_file)
+            print("== DONE")
+        elif step == 3:
+            print("== Updating Markers")
+            if xlsx_file is None:
+                print(f"xlsx_file is missing. Please include step 2 or use override")
+                return -1
+            clear_markers(all_markers)
+            transcript_data = pd.read_excel(xlsx_file, sheet_name='chapters', index_col=None,
+                                            header=0).transpose().to_dict().values()
+            insert_chapters(all_markers, transcript_data)
+            print("== DONE")
+    return 0
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser('Export audio from active premiere sequence')
+    parser.add_argument('-s', '--steps', type=int, nargs='+', help='Steps execute can be one or more\n'
+                                                                   '1: extract audio\n'
+                                                                   '2: upload/get transcript\n'
+                                                                   '3: insert markers\n'
+                                                                   'example: 1 2')
+    parser.add_argument('--id', help='Force transcript id for step 2')
+    parser.add_argument('--xlsx', help='Force xlsx file for step 3')
+    return parser.parse_args(args=argv)
+
+
+if __name__ == '__main__':
+    exit(main(sys.argv[1:]))
